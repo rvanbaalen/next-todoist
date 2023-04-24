cf5df93ee2e132aafbc820917187de3f0a1e3c0d
Robin van Baalen <robin@ganttify.com>
2023-04-24 09:25:51 -0400

    add next
    Signed-off-by: Robin van Baalen <robin@ganttify.com>

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth"
+import TodoistProvider from "next-auth/providers/todoist";
+
+export const authOptions = {
+    // Configure one or more authentication providers
+    providers: [
+        TodoistProvider({
+            clientId: process.env.TODOIST_CLIENT_ID || 'UNDEFINED',
+            clientSecret: process.env.TODOIST_CLIENT_SECRET || 'UNDEFINED'
+        })
+    ],
+}
+
+export default NextAuth(authOptions)
